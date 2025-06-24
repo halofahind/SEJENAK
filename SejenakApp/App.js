b4bbd89ad5239cfe6d7f8589914ad67e9994cd2e
halofahind <rizkyardhane01@gmail.com>
2025-06-24 07:34:22 +0700

    nav

--- SejenakApp/App.js
+++ SejenakApp/App.js
@@ -13,96 +13,63 @@ import Profil from "./screens/profil/Profil";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-
-function MainTabNavigator() {
-  return (
-    <Tab.Navigator tabBar={(props) => <CustomTabBar {...props} />}>
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="home"
-              size={24}
-              color={focused ? "#D84059" : "#aaa"}
-            />
-          ),
-          headerShown: false,
-        }}
-      />
-      <Tab.Screen
-        name="Jurnalku"
-        component={Jurnalku}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="book"
-              size={24}
-              color={focused ? "#D84059" : "#aaa"}
-            />
-          ),
-          headerShown: false,
-        }}
-      />
-      <Tab.Screen
-        name="Konseling"
-        component={Konseling}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="send"
-              size={24}
-              color={focused ? "#D84059" : "#aaa"}
-            />
-          ),
-          headerShown: false,
-        }}
-      />
-      <Tab.Screen
-        name="Profil"
-        component={Profil}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="user"
-              size={24}
-              color={focused ? "#D84059" : "#aaa"}
-            />
-          ),
-          headerShown: false,
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
-
 export default function App() {
-  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
-
-  useEffect(() => {
-    const checkLaunch = async () => {
-      const hasLaunched = await AsyncStorage.getItem("hasLaunched");
-      if (hasLaunched === null) {
-        await AsyncStorage.setItem("hasLaunched", "true");
-        setIsFirstLaunch(true);
-      } else {
-        setIsFirstLaunch(false);
-      }
-    };
-    checkLaunch();
-  }, []);
-
-  if (isFirstLaunch === null) return null; // bisa return <Splash /> atau loading
-
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {isFirstLaunch && (
-          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-        )}
-        <Stack.Screen name="Main" component={MainTabNavigator} />
-      </Stack.Navigator>
+      <Tab.Navigator tabBar={(props) => <CustomTabBar {...props} />}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="home"
+                size={24}
+                color={focused ? "#D84059" : "#aaa"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Jurnalku"
+          component={Jurnalku}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="book"
+                size={24}
+                color={focused ? "#D84059" : "#aaa"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Konseling"
+          component={Konseling}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="send"
+                size={24}
+                color={focused ? "#D84059" : "#aaa"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profil"
+          component={Profil}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="user"
+                size={24}
+                color={focused ? "#D84059" : "#aaa"}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
