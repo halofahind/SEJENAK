358fa288e257f53b851b5b0af4ae315d390637fe
Revalina Azahra <revalinaprinawan05@gmail.com>
2025-07-05 23:00:39 +0700

    tes

--- SejenakApp/App.js
+++ SejenakApp/App.js
@@ -1,19 +1,13 @@
 // App.js
 import React, { useEffect } from "react";
+import { Alert, Platform, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { StyleSheet } from "react-native";
-
-// Icons
-import Ionicons from "react-native-vector-icons/Feather";
-import { Alert, Platform } from "react-native";
 
 // Notifikasi
 import * as Notifications from "expo-notifications";
 import * as Device from "expo-device";
-
-// Screens
 
 // Screens - Start
 import SplashScreen from "./screens/Start/Splash";
@@ -21,7 +15,7 @@ import OnboardingScreen from "./screens/Start/OnBoarding";
 import MainTabs from "./screens/Start/MainTabs";
 
 // Screens - Login & Profil
-// import Login from "./screens/Login/Login";
+import Login from "./screens/Login/Login";
 import Daftar from "./screens/Login/Daftar";
 import Setting from "./screens/profil/Setting";
 import KelolaAkun from "./screens/profil/AkunSetting/KelolaAkun";
@@ -31,7 +25,6 @@ import AkunPersonal from "./screens/profil/AkunSetting/AkunPersonal";
 import DetailAkun from "./screens/profil/Detail/DetailAkun";
 import TambahAkun from "./screens/profil/TambahAkun";
 import NotifikasiScreen from "./screens/home/NotifikasiScreen";
-
 
 // Screens - Home
 import BerdamaiDenganPikiran from "./screens/home/DetailTopik/BerdamaiDenganPikiran";
@@ -47,17 +40,17 @@ import Jurnal1 from "./screens/home/JurnalSelesai/Jurnal1";
 // Screens - Konseling
 import Konseling from "./screens/konseling/Konseling";
 import DetailKonseling from "./screens/konseling/DetailKonseling";
+import Topik from "./screens/konseling/Topik";
 import TopikList from "./screens/konseling/TopikList";
 import TopikForm from "./screens/konseling/TopikForm";
-import Topik from "./screens/konseling/Topik";
 
 // Screens - Jurnalku
 import DaftarJurnal from "./screens/jurnalku/DaftarJurnal";
-import Login from "./screens/Login/Login";
 
+// Stack navigator
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 
+// Notifikasi Handler
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
@@ -73,7 +66,7 @@ export default function App() {
 
   const setupNotifications = async () => {
     await registerForPushNotificationsAsync();
-    await Notifications.cancelAllScheduledNotificationsAsync(); 
+    await Notifications.cancelAllScheduledNotificationsAsync();
     await Notifications.scheduleNotificationAsync({
       content: {
         title: "⏰ Ingatkan Jurnal Harian",
@@ -91,13 +84,14 @@ export default function App() {
       const { status: existingStatus } =
         await Notifications.getPermissionsAsync();
       let finalStatus = existingStatus;
+
       if (existingStatus !== "granted") {
         const { status } = await Notifications.requestPermissionsAsync();
         finalStatus = status;
       }
+
       if (finalStatus !== "granted") {
         Alert.alert("Gagal mendapatkan izin notifikasi!");
-        return;
       }
     } else {
       Alert.alert("Notifikasi hanya bisa di perangkat fisik.");
@@ -105,41 +99,6 @@ export default function App() {
   };
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="MainTabs"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="OnBoarding" component={OnboardingScreen} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Daftar" component={Daftar} />
-        <Stack.Screen name="Setting" component={Setting} />
-        <Stack.Screen name="MainTabs" component={MainTabs} />
-        <Stack.Screen name="Konseling" component={Konseling} />
-        <Stack.Screen name="DetailKonseling" component={DetailKonseling} />
-        <Stack.Screen name="Topik" component={Topik} />
-        <Stack.Screen name="TopikList" component={TopikList} />
-        <Stack.Screen name="KenaliDiriScreen" component={KenaliDiriScreen} />
-        <Stack.Screen name="TopikForm" component={TopikForm} />
-        <Stack.Screen
-          name="BerdamaiDenganPikiran"
-          component={BerdamaiDenganPikiran}
-        />
-        <Stack.Screen name="Pertanyaan1" component={Pertanyaan1} />
-        <Stack.Screen name="KelolaAkun" component={KelolaAkun} />
-        <Stack.Screen name="GantiPassword" component={GantiPassword} />
-        <Stack.Screen name="HapusAkun" component={HapusAkun} />
-        <Stack.Screen name="AkunPersonal" component={AkunPersonal} />
-        <Stack.Screen name="MoodSummary" component={MoodSummary} />
-        <Stack.Screen name="MoodTracker" component={MoodTracker} />
-        <Stack.Screen name="NextScreen" component={NextScreen} />
-        <Stack.Screen name="Jurnal1" component={Jurnal1} />
-        <Stack.Screen name="DetailAkun" component={DetailAkun} />
-        <Stack.Screen name="TambahAkun" component={TambahAkun} />
-        <Stack.Screen name="NotifikasiScreen" component={NotifikasiScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
         <Stack.Navigator
@@ -154,7 +113,6 @@ export default function App() {
           <Stack.Screen name="MainTabs" component={MainTabs} />
 
           {/* Home */}
-
           <Stack.Screen
             name="BerdamaiDenganPikiran"
             component={BerdamaiDenganPikiran}
@@ -170,6 +128,7 @@ export default function App() {
           <Stack.Screen name="MoodTracker" component={MoodTracker} />
           <Stack.Screen name="NextScreen" component={NextScreen} />
           <Stack.Screen name="Jurnal1" component={Jurnal1} />
+          <Stack.Screen name="NotifikasiScreen" component={NotifikasiScreen} />
 
           {/* Konseling */}
           <Stack.Screen name="Konseling" component={Konseling} />
