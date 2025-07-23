ac1b04a7af7137f40dead08ba3c58e1ffcce00f8
alvyan03 <alfianramdhan003@gmail.com>
2025-07-23 17:05:50 +0700

    heho

--- SejenakApp/utils/constants.js
+++ SejenakApp/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://172.20.10.6:8080";
+export const API_BASE_URL = "http://192.168.0.170:8080";
 //export const API_BASE_URL = "http://10.1.47.159:8080";
 // export const API_BASE_URL = "http://192.168.1.5:8080";
 //export const API_BASE_URL = "http://10.1.53.14:8080";
